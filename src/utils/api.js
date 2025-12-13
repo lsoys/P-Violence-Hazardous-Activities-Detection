288ab9850bde6b1aa5e8f3abb5a209d56897a01b
Piyush11204 <piyushkrishna11@gmail.com>
2025-12-13 10:49:05 +0530

    Update API URL to local development server

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'https://p-violence-hazardous-activities-detection.onrender.com'
+const API_BASE_URL = 'http://127.0.0.1:5000'
 
 export const violenceLabels = {
   alcohol: { color: '#FF6B6B', icon: '🍺' },
